4bfb41b1924affb3e79ebd9d87781c8f9fe55a82
terry <terry@patho.gen.nz>
2017-04-14 07:42:21 +1200

    added redux thunk middleware

--- src/store.js
+++ src/store.js
@@ -1,9 +1,12 @@
 import { combineReducers } from "redux"
+import thunk from 'redux-thunk';
 import createStore from "phenomic/lib/redux/createStore"
 // eslint-disable-next-line import/no-namespace
 import * as phenomicReducers from "phenomic/lib/redux/modules"
 
 import * as reducers from './redux';
+
+const middlewares = [thunk];
 
 const store = createStore(
   combineReducers({
@@ -11,6 +14,7 @@ const store = createStore(
     ...reducers,
   }),
   { ...(typeof window !== "undefined") && window.__INITIAL_STATE__ },
+  middlewares,
 )
 
 export default store
